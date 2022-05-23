810ef66d50ff6482fe5f6f5178823f408fe007e6
Sahil <mohammed.sahil@ssipmt.com>
2022-05-23 14:36:43 +0530

    Custom Hooks

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Button from './../../Components/Button/Button'
 export default function Home() {
 
     const dispatch = useDispatch()
 
+    const [d1, setd1] = useMyHookk()
     function onInc() {
         dispatch({ type: 'Inc' })
     }
@@ -14,6 +15,7 @@ export default function Home() {
 
     function onDecten() {
         dispatch({ type: 'Dec-ten', payload: counter + 10 })
+        setd1(d1 * 2)
     }
     const counter = useSelector(function (selector) {
         return selector.counter;
@@ -22,9 +24,21 @@ export default function Home() {
         <>
             <div>Home</div>
             <h1>{counter}</h1>
+            <h1>{d1}</h1>
             <Button onClick={onInc}>Inc</Button>
             <Button onClick={onDec}>Dec</Button>
             <Button onClick={onDecten}>Dec-Ten</Button>
         </>
     )
 }
+
+function useMyHookk() {
+    const [counter, setCounter] = useState(100)
+    useEffect(function () {
+        setTimeout(function () {
+            setCounter(500)
+        }, 5000)
+    }, [])
+    return [counter, setCounter]
+}
+
